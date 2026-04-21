f5d259c409c8fd8beb3c533ad483bd4e9aa0f970
Khang Phan <invisibledark3@gmail.com>
2026-04-22 03:05:52 +0700

    add some configuration for properties

--- src/main/java/com/kp8997/springboot/myapp/rest/FunRestController.java
+++ src/main/java/com/kp8997/springboot/myapp/rest/FunRestController.java
@@ -1,10 +1,14 @@
 package com.kp8997.springboot.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${spring.application.author.name}")
+    private String author;
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
@@ -18,5 +22,10 @@ public class FunRestController {
     @GetMapping("/rocket")
     public String getRocket() {
         return "Rocket";
+    }
+
+    @GetMapping("/author")
+    public String getAuthor() {
+        return author;
     }
 }
