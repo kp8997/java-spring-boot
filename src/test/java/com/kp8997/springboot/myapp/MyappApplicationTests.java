12e661301b17172972540b3f2387f547661e83f9
Khang Phan <invisibledark3@gmail.com>
2026-04-22 00:39:15 +0700

    update this project with spring boot setup

---
+++ src/test/java/com/kp8997/springboot/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp8997.springboot.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
