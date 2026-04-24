f38a0c718a0d77984eab58ff790671a6c0f72fd1
Khang Phan <invisibledark3@gmail.com>
2026-04-24 18:38:45 +0700

    fix pom file and implement dao

---
+++ src/main/java/com/kp8997/springboot/myapp/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.kp8997.springboot.myapp.dao;
+
+import com.kp8997.springboot.myapp.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+}
---
+++ src/main/java/com/kp8997/springboot/myapp/dao/StudentDAOImpl.java
@@ -0,0 +1,23 @@
+package com.kp8997.springboot.myapp.dao;
+
+import com.kp8997.springboot.myapp.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+    private EntityManager entityManager;
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+}
