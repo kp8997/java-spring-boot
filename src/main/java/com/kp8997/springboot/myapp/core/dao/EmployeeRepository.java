f84a50b70835ccef1eb24199287a3e205eea4e7a
Khang Phan <invisibledark3@gmail.com>
2026-04-29 01:45:39 +0700

    add pagination for properties configuration

--- src/main/java/com/kp8997/springboot/myapp/core/dao/EmployeeRepository.java
+++ src/main/java/com/kp8997/springboot/myapp/core/dao/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.kp8997.springboot.myapp.core.dao;
 
 import com.kp8997.springboot.myapp.core.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+// change default endpoint from /employees to /members for spring-boot-starter-data-rest
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
