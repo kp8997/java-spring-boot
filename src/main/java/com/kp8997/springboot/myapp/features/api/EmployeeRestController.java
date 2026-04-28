91ba746cd57dbb2dea116d7d5503b112d7491dce
Khang Phan <invisibledark3@gmail.com>
2026-04-28 23:38:00 +0700

    update with patch method for partial fields

--- src/main/java/com/kp8997/springboot/myapp/features/api/EmployeeRestController.java
+++ src/main/java/com/kp8997/springboot/myapp/features/api/EmployeeRestController.java
@@ -1,22 +1,23 @@
 package com.kp8997.springboot.myapp.features.api;
 
-import com.kp8997.springboot.myapp.common.exception.EntityErrorResponse;
 import com.kp8997.springboot.myapp.common.exception.EntityNotFoundException;
 import com.kp8997.springboot.myapp.core.entity.Employee;
 import com.kp8997.springboot.myapp.features.service.EmployeeService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeRestController {
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
+    private JsonMapper jsonMapper;
 
-    public EmployeeRestController(EmployeeService employeeService) {
+    public EmployeeRestController(EmployeeService employeeService, JsonMapper jsonMapper) {
         this.employeeService = employeeService;
+        this.jsonMapper = jsonMapper;
     }
 
     @GetMapping("")
@@ -34,17 +35,32 @@ public class EmployeeRestController {
         // save now use for both update and insert
         // set to 0 make sure it is a new object
         employee.setId(null);
-
         return employeeService.save(employee);
     }
 
     @PutMapping("")
     public Employee updateEmployee(@RequestBody Employee employee) {
-
         if (employee.getId() == null) {
           throw new EntityNotFoundException("Student id is null");
         }
         return employeeService.save(employee);
+    }
 
+    @PatchMapping("/{employeeId}")
+    public  Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> payload) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new EntityNotFoundException("Employee id not found - " + employeeId);
+        }
+
+        if (payload.containsKey("id")) {
+            throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+        }
+
+        Employee patchedEmployee = jsonMapper.updateValue(employee, payload);
+
+        return employeeService.save(patchedEmployee);
     }
 }
