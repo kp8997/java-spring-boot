191d0a3e087ba7d57a5be7a72f83ed56332785f0
Khang Phan <invisibledark3@gmail.com>
2026-04-28 23:06:18 +0700

    add exception and put method

--- src/main/java/com/kp8997/springboot/myapp/features/api/EmployeeRestController.java
+++ src/main/java/com/kp8997/springboot/myapp/features/api/EmployeeRestController.java
@@ -1,7 +1,11 @@
 package com.kp8997.springboot.myapp.features.api;
 
+import com.kp8997.springboot.myapp.common.exception.EntityErrorResponse;
+import com.kp8997.springboot.myapp.common.exception.EntityNotFoundException;
 import com.kp8997.springboot.myapp.core.entity.Employee;
 import com.kp8997.springboot.myapp.features.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,5 +36,15 @@ public class EmployeeRestController {
         employee.setId(null);
 
         return employeeService.save(employee);
+    }
+
+    @PutMapping("")
+    public  Employee updateEmployee(@RequestBody Employee employee) {
+
+        if (employee.getId() == null) {
+          throw new EntityNotFoundException("Student id is null");
+        }
+        return employeeService.save(employee);
+
     }
 }
