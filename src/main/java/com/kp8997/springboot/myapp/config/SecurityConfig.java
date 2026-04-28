a1098a85c00e3b8ffd71c52a8e6bbf9a39df0fed
Khang Phan <invisibledark3@gmail.com>
2026-04-29 04:04:06 +0700

    update with security endpoints

---
+++ src/main/java/com/kp8997/springboot/myapp/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.kp8997.springboot.myapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    // spring security won't use the user/password configured in properties
+    // use this instead
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails join = User.builder().username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails marry = User.builder().username("marry")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails tim = User.builder().username("tim")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(join, marry, tim);
+    }
+}
