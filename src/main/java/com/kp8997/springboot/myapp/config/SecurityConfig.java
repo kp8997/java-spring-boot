58d5591e8847f9b6adf982385e10e6b89142180b
Khang Phan <invisibledark3@gmail.com>
2026-04-29 04:33:57 +0700

    add security for patch

--- src/main/java/com/kp8997/springboot/myapp/config/SecurityConfig.java
+++ src/main/java/com/kp8997/springboot/myapp/config/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.kp8997.springboot.myapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -28,5 +33,21 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(join, marry, tim);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees").hasRole("ADMIN")
+        );
+                        http.httpBasic(Customizer.withDefaults());
+                        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
