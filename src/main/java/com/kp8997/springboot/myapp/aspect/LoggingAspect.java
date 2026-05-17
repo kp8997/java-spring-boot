cfdd7fdc93c3d57dd33b4ce757c20e7faf1702cc
Khang Phan <invisibledark3@gmail.com>
2026-05-17 23:39:55 +0700

    first try with aspect @Before

--- src/main/java/com/kp8997/springboot/myapp/aspect/LoggingAspect.java
+++ src/main/java/com/kp8997/springboot/myapp/aspect/LoggingAspect.java
@@ -1,4 +1,15 @@
 package com.kp8997.springboot.myapp.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+    @Before("execution(public void addAccount())")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n=======> Executing @Before the advice on addAccount");
+    }
 }
